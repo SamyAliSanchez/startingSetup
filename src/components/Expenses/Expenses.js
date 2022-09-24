a71fbb3a26c40d39722c9833e798f513bc0d6a3f
Samy Ali Sanchez <102321253+SamyAliSanchez@users.noreply.github.com>
2022-09-24 10:52:47 +0200

    module4

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -1,10 +1,11 @@
+import Card from '../UI/Card';
 import './Expenses.css';
 import ExpenseItem from './ExpenseItem';
 
 function Expenses(props) {
-    return (
-        <div className="expenses">
-        <ExpenseItem
+  return (
+    <Card className="expenses">
+      <ExpenseItem
         title={props.items[0].title}
         amount={props.items[0].amount}
         date={props.items[0].date}
@@ -24,7 +25,7 @@ function Expenses(props) {
         amount={props.items[3].amount}
         date={props.items[3].date}
       />
-</div>
-    );
+    </Card>
+  );
 }
 export default Expenses;
